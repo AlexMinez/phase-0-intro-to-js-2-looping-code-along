b6f418fb32c401b4c5c42134063353bbe6fbefab
AlexMinez <alex_minihan@outlook.com>
2022-03-16 16:38:12 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`im ${age} years old. Happy brithday to me!`)
+    
+// }
+// const gifts= ["teddybear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++){
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`)
+   
+//     }
+//     return gifts;
+// }
+
+
+
+function writeCards(cardNames,eventName){
+    const messages = []
+    for (let i = 0; i<cardNames.length; i++){
+       const message = `Thank you, ${cardNames[i]}, for the wonderful ${eventName} gift!`;
+       messages.push(message)
+    } return messages
+}
+
+
+
+
+
+
+function countDown(i){
+    i = 10
+    while (i >= 0) {
+        console.log(i--);
+    }
+
+}
+   
